9ac08320caeff4cd8f57e46cb5d554f02e7b54a9
Peiling0509 <peling70@gmail.com>
2024-11-21 16:50:07 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MainApp extends StatelessWidget {
       onDispose: onDispose,
       title: 'Safana Bekam Management App',
       theme: ThemeData(
-        useMaterial3: false,
+        useMaterial3: true,
         primarySwatch: Colors.grey,
       ),
       initialRoute: '/login',
