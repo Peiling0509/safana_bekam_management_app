63c19b52f5175e83e7ed4b06cbb393fc883eceeb
ChungCheeKwang <chungcheekwang7861@gmail.com>
2024-12-21 20:11:43 +0800

    AddCustomerFormTopBar made as component

--- lib/screen/home/add_customer_form_B.dart
+++ lib/screen/home/add_customer_form_B.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:safana_bekam_management_app/components/add_customer_form_top_bar.dart';
 import 'package:safana_bekam_management_app/components/custom_scaffold.dart';
 import 'package:safana_bekam_management_app/constant/color.dart';
 import 'package:safana_bekam_management_app/screen/login/login_screen.dart';
@@ -19,45 +20,11 @@ class _AddCustomerFormScreen_B_State extends State<AddCustomerFormScreen_B> {
             child: Container(
       color: ConstantColor.backgroundColor,
       child: Center(
-        child: Column(children: [_topAppBar(), Expanded(child:_content())]),
+        child: Column(children: [const AddCustomerFormTopBar(), Expanded(child:_content())]),
       ),
     )));
   }
 
-  //same as part A
-  Widget _topAppBar() {
-    return Container(
-      color: ConstantColor.primaryColor,
-      child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              GestureDetector(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: const Icon(
-                    Icons.arrow_back_ios,
-                    color: Colors.white,
-                    size: 28,
-                  )),
-              const Center(
-                child: Text(
-                  "Maklumat Pelanggan Baru",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(right: 28),
-              )
-            ],
-          )),
-    );
-  }
 
   Widget _content() {
     return Container(
--- lib/screen/home/add_customer_form_screen.dart
+++ lib/screen/home/add_customer_form_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:safana_bekam_management_app/components/add_customer_form_top_bar.dart';
 import 'package:safana_bekam_management_app/components/custom_scaffold.dart';
 import 'package:safana_bekam_management_app/constant/color.dart';
 import 'package:safana_bekam_management_app/screen/home/add_customer_form_B.dart';
@@ -157,43 +158,10 @@ class _AddCustomerFormScreenState extends State<AddCustomerFormScreen> {
             child: Container(
       color: ConstantColor.backgroundColor,
       child: Center(
-        child: Column(children: [_topAppBar(), Expanded(child:_content())]),
+        child: Column(
+          children: [const AddCustomerFormTopBar(), Expanded(child:_content())]),
       ),
     )));
-  }
-
-  Widget _topAppBar() {
-    return Container(
-      color: ConstantColor.primaryColor,
-      child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              GestureDetector(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: const Icon(
-                    Icons.arrow_back_ios,
-                    color: Colors.white,
-                    size: 28,
-                  )),
-              const Center(
-                child: Text(
-                  "Maklumat Pelanggan Baru",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(right: 28),
-              )
-            ],
-          )),
-    );
   }
 
  Widget _content() {
